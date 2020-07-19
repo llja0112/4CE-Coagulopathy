79411e035ccc94dafc05e0c2250ef7b4cc1c654f
Li Lianjie Anthony <llja0112@gmail.com>
2020-07-19 23:21:59 +0800

    Add multiple new models for individual outcomes

--- index.R
+++ index.R
@@ -3,6 +3,7 @@ library('dplyr')
 data <- read.csv('sample/data.csv')
 labs_data <- read.csv('sample/labs_data.csv')
 
+# Create 
 PT_summary_data <- labs_data %>%
 	filter(Test.Name == 'Prothrombin Time') %>%
 	group_by(Patient.Identifier) %>%
@@ -13,17 +14,14 @@ APTT_summary_data <- labs_data %>%
 	group_by(Patient.Identifier) %>%
 	summarise(mean_APTT = mean(Test.Value), min_APTT = min(Test.Value), max_APTT = max(Test.Value))
 
-
 DDimer_summary_data <- labs_data %>%
 	filter(Test.Name == 'D-Dimer') %>%
 	group_by(Patient.Identifier) %>%
-	# summarise(mean_PT = mean(Test.Value))
 	summarise(mean_DDimer = mean(Test.Value), min_DDimer = min(Test.Value), max_DDimer = max(Test.Value))
 
 fibrinogen_summary_data <- labs_data %>%
 	filter(Test.Name == 'Fibrinogen') %>%
 	group_by(Patient.Identifier) %>%
-	# summarise(mean_PT = mean(Test.Value))
 	summarise(mean_fibrinogen = mean(Test.Value), min_fibrinogen = min(Test.Value), max_fibrinogen = max(Test.Value))
 
 data = merge(data, PT_summary_data, by='Patient.Identifier')
--- model.R
+++ model.R
@@ -1,11 +1,35 @@
 library('glm2')
 
-# model <- glm(mmortality ~.,family=binomial(link='logit'),data=train)
+# Adjust the adverse outcome here
+# For this study, we are defining adverse outcome as death, ICU stay, mechanical ventilation, inotropic medications or ARDS
 data$outcome = data$Death | data$ICU.Stay
 data$outcome = data$outcome | data$Mechanical.Ventilation
-#data$outcome = data$outcome | data$Inotropic.Medications
-#data$outcome = data$outcome | data$Acute.Respiratory.Distress.Syndrome
+# data$outcome = data$outcome | data$Inotropic.Medications
+# data$outcome = data$outcome | data$Acute.Respiratory.Distress.Syndrome
 
 
-model <- glm(outcome ~ Age + min_PT + max_PT, data = data, family = "binomial")
+print('Adverse event model')
+model <- glm(outcome ~ Age + Gender + Thrombotic.Event + Bleeding.Event + fibrinogen_ratio + ddimer_ratio + Anticoagulation.Medications + Lopinavir.rotinavir, data = data, family = "binomial")
+print(model)
+print('')
+
+print('Death model')
+model <- glm(Death ~ Age + Gender + Thrombotic.Event + Bleeding.Event + fibrinogen_ratio + ddimer_ratio + Anticoagulation.Medications + Lopinavir.rotinavir, data = data, family = "binomial")
+print(model)
+print('')
+
+print('Mechanical Ventilation model')
+model <- glm(Mechanical.Ventilation ~ Age + Gender + Thrombotic.Event + Bleeding.Event + fibrinogen_ratio + ddimer_ratio + Anticoagulation.Medications + Lopinavir.rotinavir, data = data, family = "binomial")
+print(model)
+print('')
+
+print('Inotropic Medications')
+model <- glm(Inotropic.Medications ~ Age + Gender + Thrombotic.Event + Bleeding.Event + fibrinogen_ratio + ddimer_ratio + Anticoagulation.Medications + Lopinavir.rotinavir, data = data, family = "binomial")
+print(model)
+print('')
+
+print('Acute Respiratory Distress Syndrome')
+model <- glm(Acute.Respiratory.Distress.Syndrome ~ Age + Gender + Thrombotic.Event + Bleeding.Event + fibrinogen_ratio + ddimer_ratio + Anticoagulation.Medications + Lopinavir.rotinavir, data = data, family = "binomial")
+print(model)
+print('')
 
